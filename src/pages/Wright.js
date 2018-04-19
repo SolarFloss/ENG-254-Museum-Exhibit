2530dcf255da80c6e401af97e3779cc80ea7d051
Solar <Nicholasmrnickmorris@gmail.com>
2018-04-18 23:21:39 -0400

    final updates

--- src/pages/Wright.js
+++ src/pages/Wright.js
@@ -9,8 +9,9 @@ class Wright extends Component {
                     <span><a href="#background">BACKGROUND</a></span>
                     <span><a href="#work">HIS WORK</a></span>
                     <span><a href="#influence">INFLUENCE</a></span>
+                    <span><a href="#summary">SUMMARY</a></span>                    
                 </div>
-                <div className="section" id="#background">
+                <div className="section" id="background">
                     <h1>BACKGROUND</h1>
                     <div className="wright-image image"></div>
                     <p>
@@ -60,9 +61,50 @@ class Wright extends Component {
 
                     </p>
                 </div>
-            </div>
-        );
-    }
-}
-
-export default Wright;
+                <div className="section" id="summary">
+                    <h1>SUMMARY</h1>
+                    <p>At a very young age, the main character of this story, who is also the writer, Richard Wright, was alone and on his own. Very early on in his
+                        life his father left his mother to be with another woman. With only one person bringing income into the family, Richard found often found
+                        himself roaming around on his own. This resulted in him getting into trouble fairly often. For a brief time, Ella, Richard's mother moves
+                        the family to live with her sister Maggie, and her husband Hoskins. One day after Hoskins is murdered, and the family is threatened. Ella and 
+                        Maggie decide to move to West Helena, Arkansas, where they would combine their earnings and live a comfortable life. This is short lived, as 
+                        Maggie quickly finds a new man to move in with. Once she leaves, money becomes very tight and the Wright family struggles once again. 
+                    </p>
+                    <p>
+                        After Richard's mother has a severe stroke, she is unable to work and the family had to move in with Ella's mother in Jackson, Mississippi 
+                        where here siblings decide to help take care of her and her two children. It's decided that Richard's brother , Alan will be living with Maggie,
+                        while Richard gets to choose who he continue to live with. He decides to stay with his Uncle Clark who lives in Greenwood, Mississippi. Once
+                        discovering that there had been a boy who had died in the bedroom that he would be staying in, Richard decides to stay with his Grandmother. He
+                        quickly realizes the mistake that he has made
+                    </p>
+                    <p>
+                        Richard's grandmother was a very strict woman and was very serious about religion. This did not sit well with Richard as he had been used to a
+                        much higher degree of freedom, and he had not had much exposure to religion thus far into his life. His grandmother tries to spend a lot of her time
+                        "cleansing" Richard, as she believes that he is extremely sinful and disobedient. As an attempt to introduce religion into his life, his grandmother
+                        forces him to attend a religious school where Richard quickly discovers that he has a hard time fitting in with the rest of the students.
+                    </p>
+                    <p>
+                        Despite having a hard time fitting in, Richard quickly rises to the top of his class. This is where Richard first gets a taste of writing. At this point
+                        of his life, he writes a short story called <em>The Voodoo of Hell's Half-Acre</em>, and has it published in a local black newspaper.  After graduating
+                        Richard finds himself struggling in the working world. Richard had encountered racism in his childhood but not as aggressively as he is now. The realization
+                        of the hostility of the environment he is in, Richard decides to move to the north as soon as possible. Through a series of legal and illegal sales, he 
+                        finally saves up enough money to move to Memphis where he can find a paying job to fund further travel to Chicago.
+                    </p>
+                    <p>
+                        In Memphis, richard gets a job in an optical shop where he has a coworker named Falk who allows Richard to use his library card to checkout whatever books
+                        he wanted. This is where Richard first gets to really delve into his personal desires, and is able to address his strong thirst for knowledge. Later on
+                        Richard's brother, his mother, and her sister join him in Memphis, and it is at this point when they all decide to move to Chicagor
+                    </p>
+                    <p>
+                        In Chicago, Richard quickly finds a job, but this is short lived as the Great Depression leaves millions of people without jobs. This resulted in Richard
+                        first coming into contact with the concept of communism. He then becomes a communist party member as the thought of fighting against oppression resonates
+                        with him on a deep level. He begins to write more stories an books that revolve around the idea of revolting and breaking free of your oppressors.
+                    </p>
+                    <p>
+                        There is slightly more story to tell, and there are some details left out, but for the sake of this analysis, the more granular details are not necessary,
+                        especially since we will be focusing mostly on his childhood.
+                    </p>
+                </div> 
+            </div>); 
+            } 
+        } export default Wright; 
